11b057dce073da35d15e58c218098bb20e3d82e4
Academia de Software Libre <academia@lab01-07.(none)>
2014-10-10 15:07:58 -0430

    esto es ladila

---
+++ hola.py
@@ -0,0 +1,12 @@
+from__future__ import print_function
+
+def mi_funcion():
+    print (Hola mundo)
+
+ def nombres (nombre, apellido, edad):
+        nombre_completo= nombre + apellido + str(edad)
+       print (nombre_completo)
+        
+
+    nombres("OMAR ", "CH ", 24)
+
