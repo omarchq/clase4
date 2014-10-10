c85f00c9ce824d9e90f5dcdf6d1e83b6fde6b606
omarchq <omarchq7@gmail.com>
2014-10-10 15:13:38 -0430

    esto

---
+++ iteracion.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+lista = ['juan', 'pedro', 'luis']
+for item in lista:
+    if item[0] == 'juan':
+        print 'Hola juan'
+
+tupla = (1, 2, 3)
+for t in tupla:
+    print t
+
+for anio in range(2001, 2012):
+    print u'Año', str(anio)
