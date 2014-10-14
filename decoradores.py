ef365ad0af90d12740813bbb58f960b98d706729
omarchq <omarchq7@gmail.com>
2014-10-14 16:42:52 -0430

    clase3

---
+++ decoradores.py
@@ -0,0 +1,12 @@
+def sigma(*args):
+	return sum([i for i in args])
+
+def logger(fn):
+	def wrapper(*args):
+		for i, arg in enumerate(args):
+			print "arg: %d: %d" % (i,arg)
+			return fn(*args)
+
+		return wrapper
+		
+
