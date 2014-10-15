61605dd12472276a1e3f3de980bdd53ac2c52dd4
omarchq <omarchq7@gmail.com>
2014-10-15 16:09:08 -0430

    Con modificaciones

--- archivos.py
+++ archivos.py
@@ -1,5 +1,5 @@
-
-
+import os
+archivo="no_existe.txt"
 def decorador(fn):
 	def envoltorio():
 		print"Abriendo archivo...."
@@ -11,15 +11,38 @@ def decorador(fn):
 	return envoltorio
 
 
-
-
 @decorador
 def leer():
+	x=15 + 23
+	f=open("archivo.txt", 'a+')
+	
+	
+	f.write("Hola como estas?\n")
+	f.write("Todo bien y tu?\n")
+	f.write("Todo bien\n")
+	f.write(str(x))
 
-	f=open("archivo.txt")
-	for linea in f:
-		print linea
+	#print f.readline()
+	#for linea in f:	
+	#	print linea
 	f.close()
 
+def existe_archivo(archivo):
+	if os.path.exists(archivo):
+		print "Si existe"
+	else:
+		print "No puede encontrarlo"
+		print "Creando un archivo en eso nombre."
+		f=open("creando.txt", 'a+')
+		f.write("Hola\n")
+		f.close()
+#leer()
+#existe_archivo(archivo)
+existe_archivo(archivo)
 
-leer()
+
+
+
+#def agregar():
+	
+		
