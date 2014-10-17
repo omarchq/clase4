5097d8dd45c466d66205ed089a0589d1f0a5c2b9
omarchq <omarchq7@gmail.com>
2014-10-16 22:59:51 -0430

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,26 +1,28 @@
-mport click
+import click
 import os
 archivo=click.prompt('Ingrese nombre del archivo que quiere buscar: ')
+
 def decorador(fn):
 	def envolvedor(*args):
-		print "Iniciando Programa de busqueda de archivos\n"
+		print "Iniciando busqueda de archivo....\n"
 		fn(*args)
-		print "Fin del programa"
+		print "\nFin del programa"
 	return envolvedor
 
 @decorador	
 def existe_archivo(archivo):
 	if os.path.exists(archivo):
-		print "Si existe"
+		print "Si existe\n"
 		opcion=click.prompt('Desea A) Escribir en el archivo B) Leer el archivo')
 		if (opcion=='A' or opcion=='a'):
 			escritura=click.prompt("Coloque lo que quiere escribir en el archivo")
 			f=open(archivo, 'a+')
 			f.write(escritura)
 			f.close()
-			print "Es ha modificado el archivo" 
+			print "Se ha modificado el archivo" 
 			
 		elif (opcion=='B' or opcion=='b'):
+			print "\nMostrando lo que hay en el archivo...\n"
 			f=open(archivo)
 			for linea in f:	
 				print linea
@@ -28,11 +30,12 @@ def existe_archivo(archivo):
 
 	else:
 		print "El archivo no existe"
-		respuesta=click.prompt('Desea crear un archivo con ese nombre? (s/n): ')
-		if (respuesta=="si"):	
-			print "Creando un archivo en ese nombre..."
+		respuesta=click.prompt('\nDesea crear un archivo con ese nombre? (s/n): ')
+		if (respuesta=="S" or respuesta=="s"):	
+			print "\nCreando un archivo con ese nombre..."
 			f=open(archivo, 'a+')
 			f.close()
 
 if __name__== '__main__':
 	existe_archivo(archivo)
+	
